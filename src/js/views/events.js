7059712463ce5a7754ad9044a626cef513c787c5
marc1210 <marceloindart@gmail.com>
2019-07-27 16:18:11 +0000

    123

--- src/js/views/events.js
+++ src/js/views/events.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import "../../styles/home.scss";
+import "../../styles/events.scss";
 
-export class Home extends React.Component {
+export class Events extends React.Component {
 	render() {
 		return (
 			<div>
@@ -30,34 +30,28 @@ export class Home extends React.Component {
 											<div className="media-left">
 												<div className="panel panel-danger text-center date">
 													<div className="panel-heading month">
-														<span className="panel-title strong">Mar</span>
+														<span className="panel-title strong">Sept</span>
 													</div>
 													<div className="panel-body day text-danger">23</div>
 												</div>
 											</div>
 											<div className="media-body">
-												<h4 className="media-heading">Pulvinar Mauris Eu</h4>
-												<p>
-													Vivamus pulvinar mauris eu placerat blandit. In euismod tellus vel
-													ex vestibulum congue.
-												</p>
+												<h4 className="media-heading">Nursing Home</h4>
+												<p />
 											</div>
 										</li>
 										<li className="media">
 											<div className="media-left">
 												<div className="panel panel-danger text-center date">
 													<div className="panel-heading month">
-														<span className="panel-title strong">Jan</span>
+														<span className="panel-title strong">Nov</span>
 													</div>
 													<div className="panel-body text-danger day">16</div>
 												</div>
 											</div>
 											<div className="media-body">
-												<h4 className="media-heading">Aenean Consectetur Ultricies</h4>
-												<p>
-													Curabitur vel malesuada tortor, sit amet ultricies mauris. Aenean
-													consectetur ultricies luctus.
-												</p>
+												<h4 className="media-heading">pot lock</h4>
+												<p />
 											</div>
 										</li>
 										<li className="media">
@@ -66,15 +60,12 @@ export class Home extends React.Component {
 													<div className="panel-heading month">
 														<span className="panel-title strong all-caps">Dec</span>
 													</div>
-													<div className="panel-body text-danger day">8</div>
+													<div className="panel-body text-danger day">10</div>
 												</div>
 											</div>
 											<div className="media-body">
-												<h4 className="media-heading">Praesent Tincidunt</h4>
-												<p>
-													Sed convallis dignissim magna et dignissim. Praesent tincidunt
-													sapien eu gravida dignissim.
-												</p>
+												<h4 className="media-heading">Marcelo Bday</h4>
+												<p />
 											</div>
 										</li>
 									</ul>
